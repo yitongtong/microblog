45ec3be07c861227904bedd580fdac18ef1b6cb9
Copper <yit_copper@163.com>
2019-12-21 17:01:23 +0800

    Error Handling

--- app/forms.py
+++ app/forms.py
@@ -34,3 +34,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired()])
     about_me = TextField('个人介绍', validators=[Length(min=0, max=140)])
     submit = SubmitField('提交')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
