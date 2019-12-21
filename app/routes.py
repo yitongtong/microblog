f127bd3804f85185fc294dbef50538f76a9fd46e
Copper <Copper@MacBook-Pro.local>
2019-12-21 10:55:53 +0800

    add register

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -46,3 +46,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('注册成功')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='注册', form=form)
